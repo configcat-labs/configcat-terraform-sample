36a9b5dbf70ef85436b4b8c9e0f1caf849af7da3
Chavez Harris <codedbychavez@gmail.com>
2025-08-21 18:57:34 -0400

    Add `variables.tf` and `main.tf`

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "configcat_basic_auth_username" {
+  type = string
+  default = "INPUT_YOUR_AUTH_USERNAME_HERE"
+}
+
+variable "configcat_basic_auth_password" {
+  type = string
+  default = "INPUT_YOUR_AUTH_PASSWORD_HERE"
+}
